f2c552cfd5ec80a854b08e8cc48850befc78f29a
Kadir Yılmaz <acatay@outlook.com>
2026-02-01 17:10:28 +0300

    fix: sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 export const revalidate = 21600;
-export const dynamic = 'force-static';
+export const dynamic = 'force-dynamic';
 
 import type { MetadataRoute } from 'next';
 import { Routes } from '@/core/constants';
