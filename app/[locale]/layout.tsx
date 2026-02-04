70c485f70fb4f58e35736d657e6df7efea0ec0be
Kadir Yılmaz <acatay@outlook.com>
2026-02-04 17:27:30 +0300

    deploy

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -126,7 +126,6 @@ export default async function RootLayout({ children }: PropsWithChildren) {
                     showSpinner={false}
                     height={2}
                 />
-
                 <NextIntlClientProvider>
                     <ActivityLogger />
                     <Toaster
