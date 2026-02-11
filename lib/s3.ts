03f13a2420ba2fb707267c00eb166caa6a643c7e
Kadir Yılmaz <acatay@outlook.com>
2026-02-11 09:59:55 +0300

    feat: update s3

--- lib/s3.ts
+++ lib/s3.ts
@@ -2,7 +2,8 @@ import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 const s3Client = new S3Client({
-    region: process.env.AWS_REGION,
+    region: 'auto',
+    endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
